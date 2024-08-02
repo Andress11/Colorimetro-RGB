916affa471f4fe7307964b66b12ed5cac04f8e3d
Andress11 <andres.ceron11@hotmail.com>
2024-08-02 10:44:35 -0500

    add array option in constructor class Digita_Image

--- Color_lib/digital_image.py
+++ Color_lib/digital_image.py
@@ -4,15 +4,26 @@ import os
 
 class Digital_Image:
 
-    def __init__(self,path: str):
+    def __init__(self,path = None, array = None):
         
-        self.properties  = self._read_properties(path)
+        if array is None and path is not None:
+            self.properties = self._read_properties(path = path)
 
-    
-    def _read_properties(self,path):
-    
-        image_data_rgb = cv.imread(path)
-        name = os.path.basename(path)[:-4]
+        if path is None and array is not None:
+            self.properties = self._read_properties(array = array)
+
+        else:
+            print('ERROR')
+
+    def _read_properties(self,path = None, array = None):
+        
+        if path is not None and array is None:
+            image_data_rgb = cv.imread(path)
+            name = os.path.basename(path)[:-4]
+        if path is None and array is not None:
+            image_data_rgb = array
+            name = 'Foto tomada'    
+
         try:
             rows, columns, bands = image_data_rgb.shape
         except ValueError:
@@ -30,4 +41,3 @@ class Digital_Image:
         properties.shape = (rows,columns)
 
         return properties
-        
